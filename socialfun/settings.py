1b5a87aaafacd19ecf652a922ac23ab2b313a8d2
Ahmad Hazrati <hazrati.ahmad@gmail.com>
2023-10-02 20:52:02 +0000

    Create admin panel

--- socialfun/settings.py
+++ socialfun/settings.py
@@ -43,7 +43,7 @@ INSTALLED_APPS = [
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
-    'cloudinary_storage', 
+    'cloudinary_storage',
     'django.contrib.staticfiles',
     'cloudinary',
 
@@ -51,6 +51,7 @@ INSTALLED_APPS = [
     'event',
 
     # Others
+    'django_summernote',
     
 ]
 
@@ -139,11 +140,12 @@ USE_TZ = True
 STATIC_URL = '/static/'
 
 STATICFILES_STORAGE = 'cloudinary_storage.storage.StaticHashedCloudinaryStorage'
-STATICFILES_DIR = [os.path.join(BASE_DIR, 'static'),]
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static'), ]
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 MEDIA_URL = '/media/'
 DEFAULT_FILE_STORAGE = 'cloudinary_storage.storage.MediaCloudinaryStorage'
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
