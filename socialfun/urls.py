c9a28558c9f5207362b1d1fa4e5861b6039e5b89
Ahmad Hazrati <hazrati.ahmad@gmail.com>
2023-10-04 04:46:42 +0000

    Create view, add templates and style the templates

--- socialfun/urls.py
+++ socialfun/urls.py
@@ -19,4 +19,5 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('summernote/', include('django_summernote.urls')),
+    path('', include('event.urls'), name='event_urls'),
 ]
