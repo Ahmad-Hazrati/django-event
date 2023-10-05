57a3d38974dd23a421c4c0a4792341a02e7b9fd2
Ahmad Hazrati <hazrati.ahmad@gmail.com>
2023-10-05 16:26:44 +0000

    Add like functionality to the app

--- event/models.py
+++ event/models.py
@@ -18,18 +18,19 @@ class Category(models.Model):
 class Event(models.Model):
     name = models.CharField(max_length=200, unique=True)
     slug = models.SlugField(max_length=200, unique=True)
+    author = models.ForeignKey(User, on_delete=models.CASCADE, related_name="event_posts")
+
     description = models.TextField()
     featured_image = CloudinaryField('image', default='placeholder')
     excerpt = models.TextField(blank=True)
     date = models.DateTimeField()
     # location = models.location()
-    participant = models.ManyToManyField(User,
-                                         blank=True, related_name='events')
+    # participant = models.ManyToManyField(User, blank=True, related_name='events')
     updated = models.DateTimeField(auto_now=True)
     created_on = models.DateTimeField(auto_now_add=True)
     status = models.IntegerField(choices=STATUS, default=0)
     likes = models.ManyToManyField(User,
-                                   related_name='event_likes', blank=True)
+                                   related_name='eventpost_like', blank=True)
 
     class Meta:
         ordering = ['-created_on']
@@ -42,7 +43,7 @@ class Event(models.Model):
 
 
 class Comment(models.Model):
-    event = models.ForeignKey(Event, on_delete=models.CASCADE, related_name='comments', blank=True, null=True)
+    post = models.ForeignKey(Event, on_delete=models.CASCADE, related_name='comments')
     name = models.CharField(max_length=80)
     email = models.EmailField()
     body = models.TextField()
@@ -52,5 +53,5 @@ class Comment(models.Model):
     class Meta:
         ordering = ['created_on']
 
-        def __str__(self):
-            return f"Comment {self.body} by {self.name}"
+    def __str__(self):
+        return f"Comment {self.body} by {self.name}"
